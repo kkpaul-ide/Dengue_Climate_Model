015e19185732d23f297a1501c8877f2813fe6163
Kishor Paul <kishor.p61@gmail.com>
2022-05-20 16:30:53 +1000

    Added immigration and emigration to the model function

--- code/den_model.R
+++ code/den_model.R
@@ -12,15 +12,15 @@
 #' @return Return a dataframe containing values of each compartment
 #' at specified time points
 
-den_model <- function(project, params, data, initial_conditions, 
-                      option = "difference") {
+den_model <- function(project, params, data, initial_conditions, option = "difference") {
   
   # Extract key project and data parameters
   dt <- project$dt
   time <- project$npts
   temp <- data$temp
   hum <- data$hum
-  rain <- data$rain
+  # rain <- data$rain
+  rain <- data$rain14/14
   
   # Initialize compartments
   s_l <- rep(0,time)
@@ -55,6 +55,8 @@ den_model <- function(project, params, data, initial_conditions,
   Nh <- rep(0,time)
   
   newInfections <- rep(0,time)
+  newInfections1 <- rep(0,time)
+  newInfections2 <- rep(0,time)
   reInfections12 <- rep(0,time)
   reInfections21 <- rep(0,time)
   denMortality <- rep(0,time)
@@ -62,25 +64,27 @@ den_model <- function(project, params, data, initial_conditions,
   if (option == "difference") {
     # Then run as a difference equation
     
-    # Set-up parameters 
-    a <- 1 - exp(-a_t(temp) * dt)
-    PDR <- 1 - exp(-PDR_t(temp) * dt)
-    muv <- 1 - exp(-muv_th(temp,hum,dt) * dt)
-    LMR <- 1 - exp(-LMR_t(temp) * dt)
-    PMR <- 1 - exp(-PMR_t(temp) * dt)
+    # Calculate time-varying vectorized parameters
+    # a, PDR, LMR, PMR, EFD require conversion to daily probability
+    a <- 1 - exp(-briere(temp, params$a_T0, params$a_Tm, params$a_m, params$a_c) * dt)
+    PDR <- 1 - exp(-(briere(temp, params$PDR_T0, params$PDR_Tm, params$PDR_m, params$PDR_c)) * dt)
+    LMR <- 1 - exp(-briere(temp, params$LMR_T0, params$LMR_Tm, params$LMR_m, params$LMR_c) * dt)
+    PMR <- 1 - exp(-briere(temp, params$PMR_T0, params$PMR_Tm, params$PMR_m, params$PMR_c) * dt)
+    EFD <- 1 - exp(-briere(temp, params$EFD_T0, params$EFD_Tm, params$EFD_m, params$EFD_c) * dt)
+    
+    pMI <- params$betaMI * briere(temp, params$pMI_T0, params$pMI_Tm, params$pMI_m, params$pMI_c)
+    pIM <- params$betaIM * briere(temp, params$pIM_T0, params$pIM_Tm, params$pIM_m, params$pIM_c)
+    
+    muv <- 1 - exp(-muv_th(temp,hum,1) * dt)
     mul_tr <- 1 - exp(-mul_tr_t(temp,rain) * dt)
     mup_tr <- 1 - exp(-mup_tr_t(temp,rain) * dt)
-    EFD <- 1 - exp(-EFD_t(temp) * dt)
+    FEL <- FEL(rain)
     
     # Birth rate and death per 1000 per year
     
-    # TODO: Instead of BR calculate number of births from pop size needed and 
-    # Deaths
-    BR <- rep(1 - exp(-(pg$birthRate/(1000*360))* pg$dt),each=(pg$npts/length(pg$birthRate)))
-    DR <- rep(1 - exp(-(pg$deathRate/(1000*360))* pg$dt),each=(pg$npts/length(pg$deathRate)))
-    # BR <- rep(1 - exp(-(params$BR/(1000*360))* dt), time)
-    # DR <- rep(1 - exp(-(params$DR/(1000*360))* dt), time)
-    
+    BR <- rep(1 - exp(-(project$birthRate/(1000*360))* project$dt),each=(project$npts/length(project$birthRate)))
+    DR <- rep(1 - exp(-(project$deathRate/(1000*360))* project$dt),each=(project$npts/length(project$deathRate)))
+
     # inflow and outflow per day - adjust influx for start 
     # time of first and second strain. 
     im  <- rep(params$im, time)
@@ -126,50 +130,43 @@ den_model <- function(project, params, data, initial_conditions,
     Nl[1] <- s_l[1] + i_l1[1] + i_l2[1]
     Np[1] <- s_p[1] + i_p1[1] + i_p2[1]
     Nv[1] <- s_v[1] + e_v1[1] + e_v2[1] + i_v1[1] + i_v2[1]
-    Nh[1] <- s_h[1] + e_h1[1] + e_h2[1] + i_h1[1] + i_h2[1] + ci_h1[1] +  
-      ci_h2[1] + r_h1[1] + r_h2[1] + e_h12[1] + e_h21[1] + i_h12[1] + 
-      i_h21[1] +  r[1]
+    Nh[1] <- s_h[1] + e_h1[1] + e_h2[1] + i_h1[1] + i_h2[1] + ci_h1[1] + ci_h2[1] + r_h1[1] + r_h2[1] + e_h12[1] + e_h21[1] + i_h12[1] + i_h21[1] +  r[1]
     
     for (tt in 2:time) {
       
       # newpop[tt] <- PopSize[tt] - Nh[tt-1] - allDeaths[tt-1]
       
-      # Calculate time-varying non-vectorised parameters
-      pMItt <- params$betaMI * pMI(temp[tt], pMI_T0=params$pMI_T0, pMI_Tm=params$pMI_Tm, 
-                   pMI_m=params$pMI_m, pMI_c=params$pMI_c)
-      pIMtt <- params$betaIM * pIM(temp[tt], pIM_T0=params$pIM_T0, pIM_Tm=params$pIM_Tm, 
-                   pIM_m=params$pIM_m, pIM_c=params$pIM_c)
-      FELtt <- FEL(rain[tt])
+ 
       
       # mosquito population dynamics
-      s_l[tt] <- s_l[tt-1] + (EFD[tt]*FELtt*max((1-Nl[tt-1]/(Nh[tt-1]*cf*C_t(rain[tt]))),0)*0.5*Nv[tt-1])*(1 - params$nu*(i_v1[tt-1] + i_v2[tt-1])/Nv[tt-1]) - 2*LMR[tt]*s_l[tt-1] -  mul_tr[tt]*s_l[tt-1] 
-      i_l1[tt] <- i_l1[tt-1] + (EFD[tt]*FELtt*max((1-Nl[tt-1]/(Nh[tt-1]*cf*C_t(rain[tt]))),0)*0.5*Nv[tt-1])*params$nu*i_v1[tt-1]/Nv[tt-1] - LMR[tt]*i_l1[tt-1] - mul_tr[tt]*i_l1[tt-1]
-      i_l2[tt] <- i_l2[tt-1] + (EFD[tt]*FELtt*max((1-Nl[tt-1]/(Nh[tt-1]*cf*C_t(rain[tt]))),0)*0.5*Nv[tt-1])*params$nu*i_v2[tt-1]/Nv[tt-1] - LMR[tt]*i_l2[tt-1] - mul_tr[tt]*i_l2[tt-1]
-      s_p[tt] <- s_p[tt-1] + 2*LMR[tt]*s_l[tt-1] - 2*PMR[tt]*s_p[tt-1] - mup_tr[tt]*s_p[tt-1]
+      s_l[tt] <- s_l[tt-1] + (EFD[tt]*FEL[tt]*max((1-Nl[tt-1]/(Nh[tt-1]*cf*C_t(rain[tt]))),0)*0.5*Nv[tt-1])*(1 - params$nu*(i_v1[tt-1] + i_v2[tt-1])/Nv[tt-1]) - 2*LMR[tt]*s_l[tt-1] -  mul_tr[tt]*s_l[tt-1] 
+      i_l1[tt] <- i_l1[tt-1] + (EFD[tt]*FEL[tt]*max((1-Nl[tt-1]/(Nh[tt-1]*cf*C_t(rain[tt]))),0)*0.5*Nv[tt-1])*params$nu*i_v1[tt-1]/Nv[tt-1] - LMR[tt]*i_l1[tt-1] - mul_tr[tt]*i_l1[tt-1]
+      i_l2[tt] <- i_l2[tt-1] + (EFD[tt]*FEL[tt]*max((1-Nl[tt-1]/(Nh[tt-1]*cf*C_t(rain[tt]))),0)*0.5*Nv[tt-1])*params$nu*i_v2[tt-1]/Nv[tt-1] - LMR[tt]*i_l2[tt-1] - mul_tr[tt]*i_l2[tt-1]
+      s_p[tt] <- s_p[tt-1] + LMR[tt]*s_l[tt-1] - PMR[tt]*s_p[tt-1] - mup_tr[tt]*s_p[tt-1]
       i_p1[tt] <- i_p1[tt-1] + LMR[tt]*i_l1[tt-1] - PMR[tt]*i_p1[tt-1] - mup_tr[tt]*i_p1[tt-1]
       i_p2[tt] <- i_p2[tt-1] + LMR[tt]*i_l2[tt-1] - PMR[tt]*i_p2[tt-1] - mup_tr[tt]*i_p2[tt-1]
-      s_v[tt] <- s_v[tt-1] + 2*PMR[tt]*s_p[tt-1] - a[tt]*pMItt*((i_h1[tt-1] + params$ADE*i_h21[tt-1])+(i_h2[tt-1] + params$ADE*i_h12[tt-1]))*s_v[tt-1]/Nh[tt-1] - muv[tt]*s_v[tt-1]
-      e_v1[tt] <- e_v1[tt-1] + a[tt]*pMItt*(i_h1[tt-1] + params$ADE*i_h21[tt-1])/Nh[tt-1]*s_v[tt-1] - PDR[tt]*e_v1[tt-1] - muv[tt]*e_v1[tt-1]
-      e_v2[tt] <- e_v2[tt-1] + a[tt]*pMItt*(i_h2[tt-1] + params$ADE*i_h12[tt-1])/Nh[tt-1]*s_v[tt-1] - PDR[tt]*e_v2[tt-1] - muv[tt]*e_v2[tt-1]
+      s_v[tt] <- s_v[tt-1] + PMR[tt]*s_p[tt-1] - a[tt]*pMI[tt]*((i_h1[tt-1] + params$ADE*i_h21[tt-1])+(i_h2[tt-1] + params$ADE*i_h12[tt-1]))*s_v[tt-1]/Nh[tt-1] - muv[tt]*s_v[tt-1]
+      e_v1[tt] <- e_v1[tt-1] + a[tt]*pMI[tt]*(i_h1[tt-1] + params$ADE*i_h21[tt-1])/Nh[tt-1]*s_v[tt-1] - PDR[tt]*e_v1[tt-1] - muv[tt]*e_v1[tt-1]
+      e_v2[tt] <- e_v2[tt-1] + a[tt]*pMI[tt]*(i_h2[tt-1] + params$ADE*i_h12[tt-1])/Nh[tt-1]*s_v[tt-1] - PDR[tt]*e_v2[tt-1] - muv[tt]*e_v2[tt-1]
       i_v1[tt] <- i_v1[tt-1] + PMR[tt]*i_p1[tt-1] + PDR[tt]*e_v1[tt-1] - muv[tt]*i_v1[tt-1]
       i_v2[tt] <- i_v2[tt-1] + PMR[tt]*i_p2[tt-1] + PDR[tt]*e_v2[tt-1] - muv[tt]*i_v2[tt-1]
       
       # host population dynamics
       s_h[tt] <- s_h[tt-1] +  BR[tt]*Nh[tt-1] - # newpop[tt] -
-        a[tt]*pIMtt*((i_v1[tt-1] + i_v2[tt-1])/Nh[tt-1])*s_h[tt-1] - DR[tt]*s_h[tt-1]  + im[tt]*Nh[tt-1] - em[tt]*s_h[tt-1] 
-      e_h1[tt] <- e_h1[tt-1] + a[tt]*pIMtt*(i_v1[tt-1]/Nh[tt-1])*s_h[tt-1] - params$alpha1*e_h1[tt-1] - DR[tt]*e_h1[tt-1] + influx1[tt-1] - em[tt]*e_h1[tt-1] 
-      e_h2[tt] <- e_h2[tt-1] + a[tt]*pIMtt*(i_v2[tt-1]/Nh[tt-1])*s_h[tt-1] - params$alpha2*e_h2[tt-1] - DR[tt]*e_h2[tt-1] + influx2[tt-1] - em[tt]*e_h2[tt-1]
+        a[tt]*pIM[tt]*((i_v1[tt-1] + i_v2[tt-1])/Nh[tt-1])*s_h[tt-1] - DR[tt]*s_h[tt-1]  + im[tt]*Nh[tt-1]*(1-params$exseroprev*param_set$serofactor) - em[tt]*s_h[tt-1] 
+      e_h1[tt] <- e_h1[tt-1] + a[tt]*pIM[tt]*(i_v1[tt-1]/Nh[tt-1])*s_h[tt-1] - params$alpha1*e_h1[tt-1] - DR[tt]*e_h1[tt-1] + influx1[tt-1] - em[tt]*e_h1[tt-1] 
+      e_h2[tt] <- e_h2[tt-1] + a[tt]*pIM[tt]*(i_v2[tt-1]/Nh[tt-1])*s_h[tt-1] - params$alpha2*e_h2[tt-1] - DR[tt]*e_h2[tt-1] + influx2[tt-1] - em[tt]*e_h2[tt-1]
       i_h1[tt] <- i_h1[tt-1] + params$alpha1*e_h1[tt-1] - params$gamma1*i_h1[tt-1] - DR[tt]*i_h1[tt-1] - em[tt]*i_h1[tt-1]
       i_h2[tt] <- i_h2[tt-1] + params$alpha2*e_h2[tt-1] - params$gamma2*i_h2[tt-1] - DR[tt]*i_h2[tt-1] - em[tt]*i_h2[tt-1]
       ci_h1[tt] <- ci_h1[tt-1] + params$gamma1*i_h1[tt-1] - params$sigma1*ci_h1[tt-1] - DR[tt]*ci_h1[tt-1] - em[tt]*ci_h1[tt-1]
       ci_h2[tt] <- ci_h2[tt-1] + params$gamma2*i_h2[tt-1] - params$sigma2*ci_h2[tt-1] - DR[tt]*ci_h2[tt-1] - em[tt]*ci_h2[tt-1]
-      r_h1[tt] <- r_h1[tt-1] + params$sigma1*ci_h1[tt-1] - a[tt]*pIMtt*(i_v2[tt-1]/Nh[tt-1])*r_h1[tt-1] - DR[tt]*r_h1[tt-1] - em[tt]*r_h1[tt-1]
-      r_h2[tt] <- r_h2[tt-1] + params$sigma2*ci_h2[tt-1] - a[tt]*pIMtt*(i_v1[tt-1]/Nh[tt-1])*r_h2[tt-1] - DR[tt]*r_h2[tt-1] - em[tt]*r_h2[tt-1]
-      e_h12[tt] <- e_h12[tt-1] + a[tt]*pIMtt*(i_v2[tt-1]/Nh[tt-1])*r_h1[tt-1] - params$alpha2*e_h12[tt-1] - DR[tt]*e_h12[tt-1] - em[tt]*e_h12[tt-1]
-      e_h21[tt] <- e_h21[tt-1] + a[tt]*pIMtt*(i_v1[tt-1]/Nh[tt-1])*r_h2[tt-1] - params$alpha1*e_h21[tt-1] - DR[tt]*e_h21[tt-1] - em[tt]*e_h21[tt-1]
+      r_h1[tt] <- r_h1[tt-1] + params$sigma1*ci_h1[tt-1] - a[tt]*pIM[tt]*(i_v2[tt-1]/Nh[tt-1])*r_h1[tt-1] - DR[tt]*r_h1[tt-1] + im[tt]*Nh[tt-1]*params$exseroprev*param_set$serofactor*(r_h1[tt-1]/(r_h1[tt-1]+r_h2[tt-1]+r[tt-1])) - em[tt]*r_h1[tt-1]
+      r_h2[tt] <- r_h2[tt-1] + params$sigma2*ci_h2[tt-1] - a[tt]*pIM[tt]*(i_v1[tt-1]/Nh[tt-1])*r_h2[tt-1] - DR[tt]*r_h2[tt-1] + im[tt]*Nh[tt-1]*params$exseroprev*param_set$serofactor*(r_h2[tt-1]/(r_h1[tt-1]+r_h2[tt-1]+r[tt-1])) - em[tt]*r_h2[tt-1]
+      e_h12[tt] <- e_h12[tt-1] + a[tt]*pIM[tt]*(i_v2[tt-1]/Nh[tt-1])*r_h1[tt-1] - params$alpha2*e_h12[tt-1] - DR[tt]*e_h12[tt-1] - em[tt]*e_h12[tt-1]
+      e_h21[tt] <- e_h21[tt-1] + a[tt]*pIM[tt]*(i_v1[tt-1]/Nh[tt-1])*r_h2[tt-1] - params$alpha1*e_h21[tt-1] - DR[tt]*e_h21[tt-1] - em[tt]*e_h21[tt-1]
       i_h12[tt] <- i_h12[tt-1] + params$alpha2*e_h12[tt-1] - params$DIM*i_h12[tt-1] - params$gamma2*i_h12[tt-1] - DR[tt]*i_h12[tt-1] - em[tt]*i_h12[tt-1]
       i_h21[tt] <- i_h21[tt-1] + params$alpha1*e_h21[tt-1] - params$DIM*i_h21[tt-1] - params$gamma1*i_h21[tt-1] - DR[tt]*i_h21[tt-1] - em[tt]*i_h21[tt-1]
-      r[tt] <- r[tt-1] + params$gamma2*i_h12[tt-1] + params$gamma1*i_h21[tt-1] - DR[tt]*r[tt-1] - em[tt]*r[tt-1]
+      r[tt] <- r[tt-1] + params$gamma2*i_h12[tt-1] + params$gamma1*i_h21[tt-1] - DR[tt]*r[tt-1] + im[tt]*Nh[tt-1]*params$exseroprev*param_set$serofactor*(r[tt-1]/(r_h1[tt-1]+r_h2[tt-1]+r[tt-1])) - em[tt]*r[tt-1]
       
       # Update population size
       Nl[tt] <- s_l[tt] + i_l1[tt] + i_l2[tt]
@@ -178,9 +175,11 @@ den_model <- function(project, params, data, initial_conditions,
       Nh[tt] <- s_h[tt] + e_h1[tt] + e_h2[tt] + i_h1[tt] + i_h2[tt] + ci_h1[tt] +  ci_h2[tt] + r_h1[tt] + r_h2[tt] + e_h12[tt] + e_h21[tt] + i_h12[tt] + i_h21[tt] +  r[tt]
       
       # Extra outputs
-      newInfections[tt] <- a[tt]*pIMtt*((i_v1[tt-1] + i_v2[tt-1])/Nh[tt-1])*s_h[tt-1]
-      reInfections12[tt] <- a[tt]*pIMtt*(i_v2[tt-1]/Nh[tt-1])*r_h1[tt-1]
-      reInfections21[tt] <- a[tt]*pIMtt*(i_v1[tt-1]/Nh[tt-1])*r_h2[tt-1]
+      newInfections[tt] <- a[tt]*pIM[tt]*((i_v1[tt-1] + i_v2[tt-1])/Nh[tt-1])*s_h[tt-1]
+      newInfections1[tt] <- a[tt]*pIM[tt]*(i_v1[tt-1]/Nh[tt-1])*s_h[tt-1]
+      newInfections2[tt] <- a[tt]*pIM[tt]*(i_v2[tt-1]/Nh[tt-1])*s_h[tt-1]
+      reInfections12[tt] <- a[tt]*pIM[tt]*(i_v2[tt-1]/Nh[tt-1])*r_h1[tt-1]
+      reInfections21[tt] <- a[tt]*pIM[tt]*(i_v1[tt-1]/Nh[tt-1])*r_h2[tt-1]
       denMortality[tt] <-  params$DIM*i_h12[tt-1] + params$DIM*i_h21[tt-1]
       
       # allDeaths[tt] <- 0
@@ -191,7 +190,7 @@ den_model <- function(project, params, data, initial_conditions,
     # Store outputs from model in results
     return(tibble(time = 1:time, Nl, s_l, i_l1, i_l2, Np, s_p, i_p1, i_p2, Nv, s_v, e_v1, e_v2, i_v1, i_v2, 
                       Nh, s_h, e_h1, e_h2, i_h1, i_h2, ci_h1, ci_h2, 
-                      r_h1, r_h2, e_h12, e_h21, i_h12, i_h21, r, newInfections,
+                      r_h1, r_h2, e_h12, e_h21, i_h12, i_h21, r, newInfections, newInfections1, newInfections2,
                       reInfections12, reInfections21, denMortality))
     
   } else if (option == "ode") {
